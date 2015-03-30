573baf39c54bf1ad6aa992ea6805bfbf890c8b3e
Ben Kutil <ben@make-things.com>
2015-03-30 19:00:47 -0400

    update code for mapping example

--- app/scripts/mapping.js
+++ app/scripts/mapping.js
@@ -1,3 +1,37 @@
 (function ($) {
   var apiKey = 'AIzaSyC_fxfI_gPAJ2a8NDXk7FQk32WaKqgPYG8';
+  var getEntries;
+  var getLocations;
+  var displayEntries;
+  var resultsArr = [];
+  var allLocationsArr = [];
+
+
+  getEntries = function getEntriesF () {
+    req = $.ajax({
+      url: 'http://api.dribbble.com/shots/everyone',
+      dataType: 'jsonp',
+      type: 'GET'
+    });
+
+    req.done(getLocations);
+  };
+
+  getLocations = function getLocationsF (data) {
+    var i = 0;
+    var shots = data.shots;
+
+    for (var i = shots.length - 1; i >= 0; i--) {
+      var shot = shots[i];
+
+      console.log(shot.player.location);
+    };
+  };
+
+  displayEntries = function displayEntriesF () {
+
+  };
+
+  getEntries();
+
 })(jQuery);
